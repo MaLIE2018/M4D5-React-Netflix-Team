8e2b6b3584672ccedde48e726d7cbdb4da13c869
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-04-30 12:30:55 +0200

    +++

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,22 +1,27 @@
-import logo from "./logo.svg";
-import "./styles/css/App.css";
-import NavbarNetflix from "./components/NavbarNetflix";
-import { Container } from "react-bootstrap";
-import React from "react"
-import HarryFilms from "./data/harrypotter.json"
-import MoviePage from "./components/MoviePage"
+import logo from './logo.svg';
+import './styles/css/App.css';
+import NavbarNetflix from './components/NavbarNetflix';
+import { Container } from 'react-bootstrap';
+import React from 'react';
+import HarryFilms from './data/harrypotter.json';
+import MoviePage from './components/MoviePage';
+import NetflixFooter from './components/NetflixFooter';
 
 class App extends React.Component {
+  state = {};
 
-  state ={}
+  render() {
+    return (
+      <>
+        <Container fluid>
+          <NavbarNetflix />
+          <MoviePage />
+        </Container>
 
-
-  render() {return (
-    <Container fluid>
-      <NavbarNetflix />
-      <MoviePage/>
-    </Container>
-  );}
+        <NetflixFooter />
+      </>
+    );
+  }
 }
 
 export default App;
